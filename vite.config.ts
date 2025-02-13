1693b1e8cbe07325567b85db607e6f1450e9a7eb
Joonas Rautiola <joonas@rautiola.co>
2025-02-13 18:03:58 +0200

    Remove /tierlist/ base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  base: "/tierlist/",
+  // base: "/tierlist/",
   plugins: [solidPlugin()],
   optimizeDeps: {
     exclude: ["@modular-forms/solid"],
